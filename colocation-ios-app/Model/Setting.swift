dd3efcfce0a73a87b5ed24516e5db07e28b90099
lgvv <lgw9898@naver.com>
2022-08-21 11:52:29 +0900

    [추가] 가입한 유저의 텍스트 변경

--- colocation-ios-app/Model/Setting.swift
+++ colocation-ios-app/Model/Setting.swift
@@ -12,10 +12,16 @@ struct Setting {
 }
 
 extension Setting {
+    static private var authTitle: String {
+        UserDefaults.standard.string(forKey: StringSet.UserDefaultKey.authToken) == nil
+        ? "로그인 및 회원가입"
+        : "이미 가입한 유저입니다"
+    }
+    
     static let settings: [Setting] = [
-        Setting(title: "🍁 로그인 및 회원가입"),
-        Setting(title: "🍁 관심목록"),
-        Setting(title: "🍁 예약현황"),
+        Setting(title: "👉 \(authTitle)"),
+        Setting(title: "👉 관심목록"),
+        Setting(title: "👉 예약현황"),
 //        Setting(title: "🍁 후기작성"),
     ]
 }
